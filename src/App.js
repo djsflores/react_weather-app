d39205ec47f107c5593d1199e4e14d2876a254b9
davidDSuiza <davidflores@dsuiza.com.ar>
2022-08-18 15:31:37 -0300

    Feature: axios, usestate, useeffect and usecontext aggregates

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import Dashboard from './components/Dashboard/Dashboard';
 import SideBar from './components/SideBar/SideBar';
+import { WeatherProvider } from './providers/WeatherProvider';
 
 const App = () => {
   return (
     <>
+    <WeatherProvider>
       <SideBar />
       <Dashboard />
+    </WeatherProvider>
     </>
   );
 };
--- src/components/Dashboard/Dashboard.js
+++ src/components/Dashboard/Dashboard.js
@@ -1,9 +1,30 @@
-import React from 'react';
+import React, { useContext, useEffect, useState } from 'react';
+import axios from 'axios';
+import weatherContext from '../../providers/WeatherProvider';
 
 const Dashboard = () => {
+  const [sevenDayWeather, setSevenDayWeather] = useState([]);
+  const { weather, location } = useContext(weatherContext);
+
+  const getSevenDayWeather = async () => {
+    const { data } = await axios(`http://api.weatherapi.com/v1/forecast.json?key=6be8c28794924ed8a2a184922222905&q=${location}&days=7`);
+    setSevenDayWeather(data.forecast.forecastday);
+  };
+
+  useEffect(() => {
+    getSevenDayWeather();
+  }, [location]);
   return (
     <div>
       <h1>Dashboard</h1>
+      {
+        sevenDayWeather?.map((day, i) => <p key={i}>{day.date}</p>)
+      }
+      <h2>Today&apos;s Hightlights</h2>
+      <p>Wind status: {weather?.current?.wind_mph}</p>
+      <p>Humidity: {weather?.current?.humidity}</p>
+      <p>Visibility: {weather?.current?.vis_miles}</p>
+      <p>Air pressure: {weather?.current?.pressure_mb}</p>
     </div>
   );
 };
